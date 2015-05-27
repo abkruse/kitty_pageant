18eb2f2087f693da82b137691f9386f6893fdced
Alex Kruse <abkruse@gmail.com>
2015-05-27 12:50:16 -0700

    Add game functionality to show new images

--- kitty_pageant.js
+++ kitty_pageant.js
@@ -1,43 +1,28 @@
 var Cat = function(img, votes) {
   this.img = img;
-  this.votes = Math.floor(Math.random()*5000);
+  this.votes = 0;
 }
 
 var kittyPics = [];
-  kittyPics.push('kitty_pics/0.jpg');
-  kittyPics.push('kitty_pics/1.jpg');
-  kittyPics.push('kitty_pics/2.jpg');
-  kittyPics.push('kitty_pics/3.jpg');
-  kittyPics.push('kitty_pics/4.jpg');
-  kittyPics.push('kitty_pics/5.jpg');
-  kittyPics.push('kitty_pics/6.jpg');
-  kittyPics.push('kitty_pics/7.jpg');
-  kittyPics.push('kitty_pics/8.jpg');
-  kittyPics.push('kitty_pics/9.jpg');
-  kittyPics.push('kitty_pics/10.jpg');
-  kittyPics.push('kitty_pics/11.jpg');
-  kittyPics.push('kitty_pics/12.jpg');
-  kittyPics.push('kitty_pics/13.jpg');
+
+for (var i=0; i<14; i++){
+  kittyPics.push('kitty_pics/'+[i]+'.jpg');
+}
 
 var showCat = function(){
-  var idx = Math.floor(Math.random() * 14);
-  var idx2 = Math.floor(Math.random()*14);
-  $('.cat-picone').prepend('<img src = \" ' + kittyPics[idx] + '\">');
-  $('.cat-pictwo').prepend('<img src = \" ' + kittyPics[idx2] + '\">');
-  var votes1 = Math.floor(Math.random()*5000);
-  var votes2 = Math.floor(Math.random()*5000);
-  $('#cat-totalone').append(votes1);
-  $('#cat-totaltwo').append(votes2);
+  var idx = Math.floor(Math.random() * kittyPics.length);
+  var idx2 = Math.floor(Math.random()* kittyPics.length);
+  $('.pix-one').attr('src', kittyPics[idx]);
+  $('.pix-two').attr('src', kittyPics[idx2]);
 }
 
 showCat();
 
-$('p').hide()
-
-$('img').on('click', function(){
-  $('p').show();
+$('img').on('click', function(e){
+  e.preventDefault();
+  $('img').removeClass('winner');
   $(this).addClass('winner');
-  $('.winner').append('<p><strong>Thanks for giving me one more vote!</strong></p>')
+  this.votes =+ 1;
+  var idx3 = Math.floor(Math.random() * kittyPics.length);
+  $('img').not('.winner').attr('src', kittyPics[idx3]);
 });
-
-
